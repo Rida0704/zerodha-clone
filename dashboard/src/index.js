909e356ee5e857d7481a4712ff24fb3560c1b88b
Rida Khuraishi <ridakhuraishi@gmail.com>
2025-08-03 18:14:31 +0530

    Remove AuthWrapper from dashboard - backend handles authentication

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
 import "./index.css";
 import Home from "./components/Home";
-import AuthWrapper from "./components/AuthWrapper";
 import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -12,11 +11,9 @@ root.render(
   <React.StrictMode>
     <CookiesProvider>
       <BrowserRouter>
-        <AuthWrapper>
-          <Routes>
-            <Route path="/*" element={<Home />} />
-          </Routes>
-        </AuthWrapper>
+        <Routes>
+          <Route path="/*" element={<Home />} />
+        </Routes>
       </BrowserRouter>
     </CookiesProvider>
   </React.StrictMode>
